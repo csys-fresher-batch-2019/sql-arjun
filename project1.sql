2761f81b8888cc2e202b72a6041f62c015907158
ARJUN KUMAR A E <arjunae798@gmail.com>
2019-12-30 15:59:21 +0530

    Create project1.sql

---
+++ project1.sql
@@ -0,0 +1,21 @@
+#Course Registration
+
+## FEATURES:
+Student able to select their subjects          
+
+### Feaure 1: Student Details
+
+Query:
+
+
+```
+DROP TABLE personal_details;
+CREATE TABLE personal_details(
+names varchar2(25) NOT NULL,
+registration_number varchar2(20) ,
+branch varchar2(20) NOT NULL ,
+semester char(1) NOT NULL,
+mail_id varchar2(30) NOT NULL,
+constraint reg_pk PRIMARY KEY(registration_number)
+);
+```
